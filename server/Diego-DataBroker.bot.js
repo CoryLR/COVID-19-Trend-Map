ce5d08a59b0e4d989051dc255860c5ac30330bc9
Cory Leigh Rahman <clr@cMacBook20.local>
2020-06-19 00:01:45 -0400

    Working COVID-19 data connection and mockdata using _dev function

--- server/Diego-DataBroker.bot.js
+++ server/Diego-DataBroker.bot.js
@@ -1,7 +1,8 @@
 /* Hello, I am Diego the Data Broker. */
 
 const {
-  Client
+  Client,
+  got,
 } = getDependencies();
 
 module.exports = {
@@ -9,9 +10,11 @@ module.exports = {
     runStartupTasks();
     initDataCollectionSchedule();
   },
-  getLatestDataAndMetrics: () => {
+  getLatestDataAndMetrics: async () => {
+    let weeklyAcceleration = await getWeeklyAcceleration();
     return {
-      test: "Hello World!"
+      test: "Hello World!",
+      weeklyAcceleration: weeklyAcceleration
     }
   }
 }
@@ -19,8 +22,14 @@ module.exports = {
 /**
  * Runs on every new build
  */
-function runStartupTasks() {
-  /* Run startup tasks if needed*/
+async function runStartupTasks() {
+  /* Run startup tasks if needed */
+
+  /* Testing */
+  // getWeeklyAcceleration();
+  // getWeeklyAcceleration_dev();
+  console.log("await getWeeklyAcceleration_dev()", await getWeeklyAcceleration_dev());
+  // console.log("await getWeeklyAcceleration()", await getWeeklyAcceleration());
 }
 
 /**
@@ -29,9 +38,62 @@ function runStartupTasks() {
 function initDataCollectionSchedule() {
   // Todo
   /* `npm i cron` - this is the dependency I'll want to use to establish Diego's agenda */
+  /* JHU updates their data around midnight to 1am Et, so I should pull around 2-3am ET to be safe, and not on the hour to help even out server load */
 }
 
+async function getWeeklyAcceleration() {
 
+  const CSV_URL_JHU_US_Confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+  const csvContent = await got(CSV_URL_JHU_US_Confirmed).text();
+  const temporalEnumerationInDays = 7;
+  return getAccelerationAggregation(csvContent, temporalEnumerationInDays);
+
+}
+
+async function getWeeklyAcceleration_dev() {
+
+  let fs = require("fs");
+  let path = require('path');
+  let filePath = path.join(__dirname, '../../EXTERNAL/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv');
+  let data = fs.readFileSync(filePath, "utf8");
+  const temporalEnumerationInDays = 7;
+  const csvContent = data;
+  return getAccelerationAggregation(csvContent, temporalEnumerationInDays);
+
+  //   , {
+  //   encoding: 'utf-8'
+  // }, function (err, data) {
+  //   if (!err) {
+  //     const temporalEnumerationInDays = 7;
+  //     const csvContent = data;
+  //     return getAccelerationAggregation(csvContent, temporalEnumerationInDays);
+  //   } else {
+  //     return [false, responseMessage]
+  //   }
+  // });
+}
+
+function getAccelerationAggregation(csvContent, temporalEnumerationInDays) {
+
+  /* Crunch numbers */
+
+
+  return {
+    "temporalEnumerationInDays": temporalEnumerationInDays,
+    "typeof_csvContent": typeof csvContent,
+    "csvContent_length": csvContent.length,
+  }
+}
+
+function getWebFileContent(URL, callBack) {
+  request.get(URL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callBack(body, response);
+    } else {
+      callBack(false, error);
+    }
+  });
+}
 
 /**
  * Gets all dependencies used by Diego
@@ -40,8 +102,11 @@ function getDependencies() {
   const {
     Client
   } = require('pg');
+  const got = require('got');
+
 
   return {
-    Client
+    Client,
+    got,
   };
 }
