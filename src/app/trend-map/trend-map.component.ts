350f4b916264d70ebef2f60c1b5b4510261ff457
Cory Leigh Rahman <clr@cMacBook20.local>
2020-06-28 08:43:41 -0400

    Add Leaflet and initialize map

--- src/app/trend-map/trend-map.component.ts
+++ src/app/trend-map/trend-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-trend-map',
@@ -8,14 +9,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TrendMapComponent implements OnInit {
 
+  map: any;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // this.getData();
+    this.map = this.initializeMap();
+  }
+
+  getData() {
     const url = '/api/getData';
     const body = {test: "test"};
     this.http.post(url, body).subscribe((data: any) => {
       console.log(url, data);
     });
+  }
+
+  initializeMap() {
+    const map = L.map('map', {
+      attributionControl: false,
+    }).setView([40, -98.5], 4);
+
+    /* openStreetMap as backup basemap */
+    const OpenStreetMap =  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { });
+    map.addLayer(OpenStreetMap);
+    return map;
   }
 
 }
